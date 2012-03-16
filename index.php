08d97bfc1c22bc6612bafbdd80cfc74d25d6200d
George Harnish <lefou23@gmail.com>
2012-03-16 00:11:23 -0500

    minor cosmetic changes

--- index.php
+++ index.php
@@ -1,15 +1,44 @@
 <html>
 
-<head>
-  <title>Squire</title>
-</head>
+    <head>
+	<title>Squire</title>
+    </head>
 
-<body>
-  <p>Welcome to Squire! What is your name?</p>
-  <form action="echo.php" method="post">
-    <p>Your name: <input type="text" name="name" /></p>
-    <p><input type="submit" /></p>
-  </form>
+    <body>
+
+	<!--Need to add CSS styles.-->
+
+    <center>
+	<div style="background-color:#cccccc">
+	    <h2>Welcome to Squire's Quisitive Uniformly Informational Requirements Engine!</h2>
+	    <p>Squire, for short.</p>
+	    <div
+		xmlns:cc="http://creativecommons.org/ns#"
+		xmlns:dct="http://purl.org/dc/terms/"
+		about="http://s0.geograph.org.uk/geophotos/01/58/68/1586875_86f70937.jpg">
+		<img src="./images/MartyrdomOfStMauriceAndTheTheban-ElGreco.jpg"
+		     alt="Martyrdom of St. Maurice and the Theban by El Greco"
+		     height="216" width="288">
+		<p><a rel="cc:attributionURL"
+		      property="cc:attributionName"
+		      href="http://www.fotopedia.com/items/flickr-248295616">El Greco</a>
+		</p>
+		<a rel="license" href="http://creativecommons.org/licenses/by/2.0/">
+		    <img src="./images/by.png"
+			 alt="Creative Commons Attribution 2.0"
+			 height="28" width="81">
+		</a>
+
+	    </div>
+	</div>
+    </center>
+
+    <p>What is your name?</p>
+    <form action="echo.php" method="post">
+	<p>Your name: <input type="text" name="name" /></p>
+	<p><input type="submit" /></p>
+    </form>
+    <!--<p>(not your quest or favorite color)</p>-->
 </body>
 
 </html>
